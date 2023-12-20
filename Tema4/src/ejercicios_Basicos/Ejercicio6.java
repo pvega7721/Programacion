ae2ac0e83ba5d2477c01047aecdf63b25e3ea4c5
pvega7721 <pvega7721@ceuandalucia.es>
2023-12-20 16:53:26 +0100

    .

--- Tema4/src/ejercicios_Basicos/Ejercicio6.java
+++ Tema4/src/ejercicios_Basicos/Ejercicio6.java
@@ -1,14 +1,11 @@
 package ejercicios_Basicos;
 
-public class Ejercicio5 {
+public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println(sumar(3,4));
-		System.out.println(sumar(3,0));
+		System.out.println(sumar(4,5));
 	}
-	
 	public static Integer sumar (Integer num1, Integer num2) {
 		return num1 + num2;
 	}
