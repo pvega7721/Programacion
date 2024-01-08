91fc93ac8b3ec182600aa920fd4b77faf0d711ee
pvega7721 <pvega7721@ceuandalucia.es>
2024-01-08 16:21:43 +0100

    .

--- Tema4/src/ejercicio18/Alumno.java
+++ Tema4/src/ejercicio18/Alumno.java
@@ -1,8 +1,41 @@
 package ejercicio18;
 
 public class Alumno {
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Double getNota() {
+		return nota;
+	}
+
+	public void setNota(Double nota) {
+		this.nota = nota;
+	}
+
 	private String dni; 
 	private String nombre;
 	private int edad;
-	private int nota;
+	private Double nota;
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	public void setDni(String dniNuevo) {
+		this.dni=dniNuevo;
+	}
+	
 }
--- Tema4/src/ejercicio18/App.java
+++ Tema4/src/ejercicio18/App.java
@@ -1,10 +1,28 @@
 package ejercicio18;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Alumno alumno = new Alumno();
+		//alumno.dni="45382950v";
+		//alumno.nombre="Pablo";
+		System.out.println("Introduce tu DNI");
+		String dni =sc.nextLine();
+		alumno.setDni(dni);
+		
+		System.out.println("Introduce tu edad");
+		Integer edad = sc.nextInt();
+		alumno.setEdad(edad);
+		
+		System.out.println("Introduce tu nombre");
+		String nombre = sc.nextLine();
+		alumno.setNombre(nombre);
 	}
 
 }
