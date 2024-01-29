729a010297d6fbfd5c36609c31ececcd5830809f
pvega7721 <pvega7721@ceuandalucia.es>
2024-01-29 11:51:29 +0100

    .

---
+++ Tema4/src/caballo_practica/Repaso.java
@@ -0,0 +1,13 @@
+package caballo_practica;
+
+public class Repaso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mensaje = "Hola, ¿Cómo estás?";
+		Boolean haceSol = true;
+		int dias = 7;
+		System.out.println(mensaje);
+	}
+
+}
---
+++ Tema4/src/caballo_practica/caballo.java
@@ -0,0 +1,5 @@
+package caballo_practica;
+
+public class caballo {
+	
+}
