99dfc3e8e36a388693cc447b76920896f4ee30c4
pvega7721 <pvega7721@ceuandalucia.es>
2024-02-19 15:41:27 +0100

    .

--- Tema4/src/ejercicio53/Hucha.java
+++ Tema4/src/ejercicio53/Hucha.java
@@ -6,7 +6,12 @@ import java.text.DecimalFormat;
 
 public class Hucha {
 	//Se inicia a 0 redondeando 2 decimales
-	private BigDecimal ahorro = BigDecimal.ZERO.setScale(2, RoundingMode.FLOOR);
+	private BigDecimal ahorro;
+	
+	//El valor se declara en el constructor
+	public Hucha() {
+		this.ahorro = BigDecimal.ZERO;
+	}
 	
 	//añado método get para poder acceder a el desde el App
 	public BigDecimal getAhorro() {
@@ -17,20 +22,23 @@ public class Hucha {
 	public BigDecimal meterDinero(BigDecimal introducir) {
 		//El método debe igualarse a la suma de ahorro + introducir.
 		//añado setScale a la variable introducir para que de el formato deseado
-		return ahorro = ahorro.add(introducir.setScale(2,RoundingMode.FLOOR));
+		this.ahorro = this.ahorro.add(introducir).setScale(2,RoundingMode.HALF_DOWN);
+		return this.ahorro;
 	}
 	
 	//Método que recibe el importe a sacar y devuelve el importe sacado, no hay suficiente, devuelve lo que haya.
 	public BigDecimal sacarDinero(BigDecimal sacar) {
-		if(ahorro.compareTo(sacar) == -1) {
+		
+		if(ahorro.compareTo(sacar) <= 0) {
 			BigDecimal ahorroActual = ahorro;
-			ahorro = ahorro.subtract(ahorro);
+			ahorro = BigDecimal.ZERO;
 			return ahorroActual;
-		}else {
-		//El método debe igualarse a la resta de ahorro + sacar.
-		//Añado setScale a la variable sacar para que de el formato deseado.
-		return ahorro = ahorro.subtract(sacar.setScale(2,RoundingMode.FLOOR));
 		}
+		//El método debe igualarse a ahorro - sacar.
+		//Añado setScale a la variable sacar para que de el formato deseado.
+			ahorro = ahorro.subtract(sacar).setScale(2,RoundingMode.HALF_DOWN);
+		return sacar;
+		
 	}
 	
 	//Método que devuelve lo que hay en la hucha.
@@ -40,15 +48,15 @@ public class Hucha {
 	//Método romper hucha que saca todo el dinero que haya y devuevle lo que había
 	public BigDecimal romperHucha() {
 		BigDecimal ahorro_ahora=ahorro;
-		ahorro = ahorro.subtract(ahorro);
+		ahorro = BigDecimal.ZERO;
 		return ahorro_ahora;
 	}
 
 	//Imprimir importe actual de la hucha con un formato determinado
 	@Override
 	public String toString() {
-		DecimalFormat formato = new DecimalFormat("#,###.00€");
-		return "Hucha [ahorro= " + formato.format(ahorro) + "]";
+		DecimalFormat formato = new DecimalFormat("#,##0.00€");
+		return formato.format(ahorro);
 	}
 	
 }
