6244726dde463dbdea120907291d482bb34fe967
pvega7721 <pvega7721@ceuandalucia.es>
2024-03-20 17:00:26 +0100

    .

---
+++ Tema5/src/ejercicio56/App.java
@@ -0,0 +1,34 @@
+package ejercicio56;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Map<Integer, Integer> numeros = new HashMap<>();
+
+		Integer num = 1;
+
+		do {
+			System.out.println("Introduce un número entero");
+			num = sc.nextInt();
+			if(!num.equals(0)) {
+				
+				if (numeros.containsKey(num)) {
+					Integer cantidadActual = numeros.get(num);
+					numeros.put(num, cantidadActual +1);
+				} else {
+					numeros.put(num, 1);
+				}
+			}
+			
+		} while (!num.equals(0));
+
+		sc.close();
+	}
+
+}
