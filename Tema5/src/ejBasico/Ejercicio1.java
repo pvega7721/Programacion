9885ca46223530da5cfb0ecb2d5d6d3a5c83cda6
pvega7721 <pvega7721@ceuandalucia.es>
2024-04-01 16:49:40 +0200

    .

---
+++ Tema5/src/ejBasico/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejBasico;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Integer num = 0;
+		do {
+			
+		}while(num == -1);
+	}
+
+}
