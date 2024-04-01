7c1bf1fd6061ace0418bdaa629770cfed1b8fd56
pvega7721 <pvega7721@ceuandalucia.es>
2024-04-01 17:56:06 +0200

    .

--- Tema5/src/ejBasico/Ejercicio1.java
+++ Tema5/src/ejBasico/Ejercicio1.java
@@ -1,5 +1,8 @@
 package ejBasico;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ejercicio1 {
@@ -8,9 +11,34 @@ public class Ejercicio1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Integer num = 0;
-		do {
+		List<Integer> listaNumeros = new ArrayList<>();
+
+		try {
 			
-		}while(num == -1);
+			while (true) {
+				System.out.println("Introduce un número");
+				num = sc.nextInt();
+
+				if (num != -1) {
+					listaNumeros.add(num);
+				} else {
+					System.out.println("Fin de programa");
+					break;
+				}
+			}
+
+			for (Integer integer : listaNumeros) {
+				System.out.println(integer);
+			}
+			
+			
+		} catch (InputMismatchException e) {
+			System.out.println("Has introducido un caracter inválido");
+		} finally {
+			sc.close();
+			System.out.println("Scanner cerrado correctamente");
+		}
+	
 	}
 
 }
