f2aff65e2ef0f0f4a303430025fe897703beb43d
pvega7721 <pvega7721@ceuandalucia.es>
2024-05-01 14:57:15 +0200

    .

---
+++ Tema6_/src/ej1Repaso/app/App.java
@@ -0,0 +1,35 @@
+package ej1Repaso.app;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+import ej1Repaso.modelo.Persona;
+import ej1Repaso.services.PersonasService;
+
+public class App {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+
+		// Creamos una instancia de la clase PersonaService para llamar a sus métodos.
+		PersonasService service = new PersonasService();
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			// Creo instancia de persona llamando al método
+			Persona p = service.consultarPersona();
+			// Si el método devuelve null(No hay persona) imprime un mensaje de error
+			if (p == null) {
+				System.out.println("No exste persona con los datos solicitados");
+				// En caso de que si haya persona, la imprime
+			} else {
+				System.out.println(p);
+			}
+		} catch (SQLException e) {
+			System.out.println("Excepción en mail: " + e.getMessage());
+		} finally {
+			sc.close();
+		}
+
+	}
+
+}
